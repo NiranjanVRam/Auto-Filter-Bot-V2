d3946762b79f5563c2bcf34586d090dde6369318
NiranjanVRam <83854040+NiranjanVRam@users.noreply.github.com>
2021-07-28 20:22:40 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,13 +3,10 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
+I'm a bot made to work for FH Group :D
 
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
+I can provide files in my group!!
+I will work only in my group!! So don't think of adding me in your groups!
 
 For more click <i>help</i></b>"""
 
@@ -17,20 +14,19 @@ For more click <i>help</i></b>"""
     HELP_MSG = """<b>How to use the bot??</b>
 
 <i>
-* Add bot to your group with admin rights.
+* Add bot to your group with admin rights. But you cannot add me in your own group..
 
 * Add bot to channels which you want to link with <b>all admin rights</b>!
 </i>
 
-
-<b>Bot Commands - Works in Group only</b>
+<b>Bot Commands - Works in FH Group only</b>
 (You need to be a Auth User in order to use these commands)
 
 * <code>/add channelid</code>  -  Links channel to your group.
 or
 * <code>/add @channelusername</code> - Links channel to your group.
 
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+<i>NOTE : You can get your channel ID from channel details </i>
 
 
 * <code>/del channelid</code>  -  Delinks channel from group
@@ -53,17 +49,17 @@ No need add each filter again!
 Bot will automatically search for your files and give links to that!
 
 
-<b>© @TroJanzHEX</b>"""
+<b>© @fileshomeofficial</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : Jessie</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @liqwid_x
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>Join My Channel :</b> @fileshomeofficial 
 
 """
